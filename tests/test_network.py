9b582b57c462671673859c239866176df913016f
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 05:08:20 +0700

    Update test_network.py

--- tests/test_network.py
+++ tests/test_network.py
@@ -1,1 +1,21 @@
+import pytest
+from src.blockchain.network.p2p import P2PNetwork
+from src.blockchain.utils.config import ConfigLoader
 
+@pytest.fixture
+def test_config():
+    config = ConfigLoader()
+    config.config = {
+        'network': {
+            'listen_addr': '/ip4/127.0.0.1/tcp/0',
+            'bootstrap_nodes': []
+        }
+    }
+    return config
+
+@pytest.mark.asyncio
+async def test_p2p_bootstrapping(test_config):
+    network = P2PNetwork(test_config)
+    await network.start()
+    assert len(network.peers) == 0
+    await network.stop()
