792a315ae08361d0cac812b6163949d7a6a7929e
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 04:37:58 +0700

    Delete src directory

--- src/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    DIFFICULTY = int(os.getenv("DIFFICULTY", 4))
-    PORT = int(os.getenv("PORT", 8080))
-    PEERS = os.getenv("PEERS", "").split(",")
-    MINING_REWARD = 10.0
-    VERSION = "1.0.0-future"
