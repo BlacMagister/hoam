2d2a6c50b94e7ba1f07fc54d0701796f6b72532c
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 05:00:10 +0700

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
+import asyncio
+from .blockchain.core.chain import Blockchain
+from .blockchain.network.p2p import P2PNetwork
+from .blockchain.api.rest import run_api
 
+async def main():
+    blockchain = Blockchain()
+    network = P2PNetwork(config={
+        'listen_addr': '/ip4/0.0.0.0/tcp/4001',
+        'bootstrap_nodes': []
+    })
+    
+    await network.start()
+    run_api()
+
+if __name__ == '__main__':
+    asyncio.run(main())
