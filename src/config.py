ed17032fe54d31f9be3f314b88ef6552bf26ce7c
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 04:15:22 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
-DIFFICULTY = 4
-PORT = 5000
-NODES = []
+import os
+
+class Config:
+    DIFFICULTY = int(os.getenv("DIFFICULTY", 4))
+    PORT = int(os.getenv("PORT", 8080))
+    PEERS = os.getenv("PEERS", "").split(",")
+    MINING_REWARD = 10.0
+    VERSION = "1.0.0-future"
