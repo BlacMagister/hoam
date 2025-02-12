7ce70eba5fabf1a81e5038754fe8d07966c908f8
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 04:08:18 +0700

    Update blockchain.py

--- src/blockchain.py
+++ src/blockchain.py
@@ -1,6 +1,5 @@
 import hashlib
 import time
-import json
 
 class Block:
     def __init__(self, index, timestamp, data, previous_hash, nonce=0):
@@ -10,48 +9,36 @@ class Block:
         self.previous_hash = previous_hash
         self.nonce = nonce
         self.hash = self.calculate_hash()
-
+    
     def calculate_hash(self):
-        block_data = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}{self.nonce}"
-        return hashlib.sha256(block_data.encode()).hexdigest()
-
-    def to_dict(self):
-        return {
-            "index": self.index,
-            "timestamp": self.timestamp,
-            "data": self.data,
-            "previous_hash": self.previous_hash,
-            "hash": self.hash,
-            "nonce": self.nonce
-        }
+        data_string = f"{self.index}{self.timestamp}{self.data}{self.previous_hash}{self.nonce}"
+        return hashlib.sha256(data_string.encode()).hexdigest()
 
 class Blockchain:
     def __init__(self):
         self.chain = [self.create_genesis_block()]
-        self.difficulty = 4  # Ubah difficulty sesuai keinginan
+        self.difficulty = 4
     
     def create_genesis_block(self):
-        return Block(0, time.time(), "Genesis Block", "0")
+        return Block(0, time.time(), "Genesis", "0")
     
     def add_block(self, data):
-        latest_block = self.chain[-1]
         new_block = Block(
-            index=len(self.chain),
-            timestamp=time.time(),
-            data=data,
-            previous_hash=latest_block.hash
+            len(self.chain),
+            time.time(),
+            data,
+            self.chain[-1].hash
         )
-        new_block = self.proof_of_work(new_block)
+        self.mine_block(new_block)
         self.chain.append(new_block)
         return new_block
     
-    def proof_of_work(self, block):
-        while not block.hash.startswith('0' * self.difficulty):
+    def mine_block(self, block):
+        while block.hash[:self.difficulty] != "0" * self.difficulty:
             block.nonce += 1
             block.hash = block.calculate_hash()
-        return block
     
-    def is_valid(self):
+    def validate(self):
         for i in range(1, len(self.chain)):
             current = self.chain[i]
             previous = self.chain[i-1]
