eec639da82f910369f9ef9123c6d67d0de5d17e1
@DustVoid <161425524+BlacMagister@users.noreply.github.com>
2025-02-13 04:59:40 +0700

    Update keystore.py

--- src/blockchain/wallet/keystore.py
+++ src/blockchain/wallet/keystore.py
@@ -1,81 +1,43 @@
 import os
 import json
 from base64 import b64encode, b64decode
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
+from hashlib import scrypt
 
 class SecureKeystore:
-    def __init__(self, path: str = "vault.dat"):
+    def __init__(self, path: str = "wallet.keystore"):
         self.path = path
-        self._vault = self._load_vault()
-        self._current_key = None
-    
-    def _derive_key(self, password: str, salt: bytes) -> bytes:
-        kdf = PBKDF2HMAC(
-            algorithm=hashes.SHA3_256(),
-            length=32,
-            salt=salt,
-            iterations=1000000,
-            backend=default_backend()
+        self._master_key = None
+        self.keys = {}
+
+    def initialize(self, password: str):
+        salt = os.urandom(16)
+        key = scrypt(password.encode(), salt=salt, n=16384, r=8, p=1, dklen=32)
+        self._master_key = Fernet(b64encode(key))
+        self._save_keystore(salt)
+
+    def _save_keystore(self, salt: bytes):
+        data = {
+            "salt": b64encode(salt).decode(),
+            "keys": {k: self._master_key.encrypt(v).decode() 
+                    for k, v in self.keys.items()}
+        }
+        with open(self.path, 'w') as f:
+            json.dump(data, f)
+
+    def generate_keypair(self):
+        private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
+        public_key = private_key.public_key().public_bytes(
+            encoding=serialization.Encoding.X962,
+            format=serialization.PublicFormat.CompressedPoint
         )
-        return kdf.derive(password.encode())
-    
-    def _load_vault(self) -> dict:
-        if os.path.exists(self.path):
-            with open(self.path, "rb") as f:
-                return json.loads(f.read())
-        return {"keys": {}}
-    
-    def _save_vault(self):
-        with open(self.path, "wb") as f:
-            f.write(json.dumps(self._vault).encode())
-    
-    def unlock(self, password: str):
-        salt = b64decode(self._vault["salt"])
-        key = self._derive_key(password, salt)
-        self._current_key = Fernet(b64encode(key))
-    
-    def generate_keypair(self, password: str, alias: str):
-        if not self._current_key:
-            raise Exception("Vault locked")
-        
-        private_key = ec.generate_private_key(ec.SECP256K1())
-        public_key = private_key.public_key()
-        
-        pem = private_key.private_bytes(
+        self.keys[public_key.hex()] = private_key.private_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PrivateFormat.PKCS8,
             encryption_algorithm=serialization.NoEncryption()
         )
-        
-        encrypted_pem = self._current_key.encrypt(pem)
-        self._vault["keys"][alias] = {
-            "public": public_key.public_bytes(
-                serialization.Encoding.X962,
-                serialization.PublicFormat.CompressedPoint
-            ).hex(),
-            "private": b64encode(encrypted_pem).decode()
-        }
-        self._save_vault()
-    
-    def sign_transaction(self, alias: str, data: dict) -> str:
-        if alias not in self._vault["keys"]:
-            raise Exception("Key alias not found")
-        
-        encrypted_pem = b64decode(self._vault["keys"][alias]["private"])
-        decrypted_pem = self._current_key.decrypt(encrypted_pem)
-        private_key = serialization.load_pem_private_key(
-            decrypted_pem,
-            password=None,
-            backend=default_backend()
-        )
-        
-        signature = private_key.sign(
-            json.dumps(data, sort_keys=True).encode(),
-            ec.ECDSA(hashes.SHA3_256())
-        )
-        return signature.hex()
+        self._save_keystore()
+        return public_key.hex()
